54261184a21b94caa8546364cb936d1b53a9655f
Saransh-mittal <20ucs174@lnmiit.ac.in>
2023-07-16 13:39:10 +0530

    timeline made

---
+++ client/src/components/Timeline.jsx
@@ -0,0 +1,46 @@
+import TimelineItem from "./TimelineItem";
+
+const Timeline = () => {
+  return (
+    <div>
+      <div className="timeline-container">
+        <div className="row item-container">
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+          <div className="col-md-6 col-lg-4 item">
+            <TimelineItem />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Timeline;
---
+++ client/src/components/TimelineItem.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card";
+const TimelineItem = () => (
+    <div className="timeline-item">
+        <div className="timeline-item-content">
+            <Card/>
+        </div>
+    </div>
+);
+
+export default TimelineItem;
