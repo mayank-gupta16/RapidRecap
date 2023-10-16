3d127ba6b88507239babb9621a38698d73fa1a96
Saransh-mittal <20ucs174@lnmiit.ac.in>
2023-10-16 17:03:36 +0530

    updated class to className in about.jsx

--- client/src/screens/About.jsx
+++ client/src/screens/About.jsx
@@ -9,13 +9,13 @@ export default function About() {
         href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
       />
 
-      <div class="about mt-3">
+      <div className="about mt-3">
         <h1>Our Team</h1>
         <p>Meet our awesome team!</p>
       </div>
-      <section class="container about">
+      <section className="container about">
         <figure>
-          <div class="layer">
+          <div className="layer">
             <h3>Full Stack Web Developer</h3>
             <p>
               I developed the UI and back-end of RapidRecap using MERN Stack.
@@ -27,21 +27,21 @@ export default function About() {
             <br />
             <a
               href="https://www.linkedin.com/in/subratgupta2704"
-              class="fa fa-linkedin fa-2x"
+              className="fa fa-linkedin fa-2x"
             ></a>
             <a
               href="https://www.instagram.com/subrat.gupta.2704/"
-              class="fa fa-instagram fa-2x mx-3"
+              className="fa fa-instagram fa-2x mx-3"
             ></a>
             <a
               href="https://github.com/subratgupta2704"
-              class="fa fa-github fa-2x"
+              className="fa fa-github fa-2x"
             ></a>
           </figcaption>
         </figure>
 
         <figure>
-          <div class="layer">
+          <div className="layer">
             <h3>Team Lead</h3>
             <p>
               I developed the UI and back-end of RapidRecap using MERN Stack.
@@ -53,21 +53,21 @@ export default function About() {
             <br />
             <a
               href="https://www.linkedin.com/in/saransh-mittal-824927248/"
-              class="fa fa-linkedin fa-2x"
+              className="fa fa-linkedin fa-2x"
             ></a>
             <a
               href="https://www.instagram.com/saransh._.mittal/"
-              class="fa fa-instagram fa-2x mx-3"
+              className="fa fa-instagram fa-2x mx-3"
             ></a>
             <a
               href="https://github.com/Saransh-mittal"
-              class="fa fa-github fa-2x"
+              className="fa fa-github fa-2x"
             ></a>
           </figcaption>
         </figure>
 
         <figure>
-          <div class="layer">
+          <div className="layer">
             <h3>Data Scientist</h3>
             <p>I developed python servers to do web scrapping.</p>
           </div>
@@ -77,15 +77,15 @@ export default function About() {
             <br />
             <a
               href="https://www.linkedin.com/in/mayank-gupta-7104a21a6/"
-              class="fa fa-linkedin fa-2x"
+              className="fa fa-linkedin fa-2x"
             ></a>
             <a
               href="https://www.instagram.com/mkgupta.exe/"
-              class="fa fa-instagram fa-2x mx-3"
+              className="fa fa-instagram fa-2x mx-3"
             ></a>
             <a
               href="https://github.com/mayank-gupta16"
-              class="fa fa-github fa-2x"
+              className="fa fa-github fa-2x"
             ></a>
           </figcaption>
         </figure>
