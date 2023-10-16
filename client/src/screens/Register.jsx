21ea6ef095503c85194cdbe63f40f352c2f6afc2
Saransh-mittal <20ucs174@lnmiit.ac.in>
2023-10-16 18:16:21 +0530

    Frontend login/register connected with backend

--- client/src/screens/Register.jsx
+++ client/src/screens/Register.jsx
@@ -1,41 +1,75 @@
-import React from "react";
+import { useState } from "react";
 import "./Register.css";
-
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 export default function Register() {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    password: "",
+    cpassword: "",
+  });
+  const navigate = useNavigate();
+
+  const inputHandler = (e) => {
+    const {name,value} = e.target;
+    setData({...data,[name]:value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      //console.log(data);
+      const response = await axios.post(`/api/register`,data);
+      if(response.status===201)
+      {
+        alert("Registered Successfully");
+        navigate("/signin");
+      }
+      else{
+        alert("Registration Failed");
+        throw new Error("Registration Failed");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+    
+  };
   return (
     <div>
-      <body>
         <section className="container">
           <div className="r-container">
             <div className="circle circle-one"></div>
             <div className="form-container">
               <h1 className="opacity">Welcome!</h1>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="row">
                   <div className="col">
-                    <input type="text" placeholder="First Name" />
+                    <input name="firstName" onChange={inputHandler} required value={data.firstName} type="text" placeholder="First Name" />
                   </div>
                   <div className="col">
-                    <input type="text" placeholder="Last Name" />
+                    <input name="lastName" onChange={inputHandler} required value={data.lastName} type="text" placeholder="Last Name" />
                   </div>
                 </div>
                 <div className="row">
                   <div className="col">
-                    <input type="text" placeholder="Email ID" />
+                    <input name="email" onChange={inputHandler} required value={data.email} type="email" placeholder="Email ID" />
                   </div>
                   <div className="col">
-                    <input type="text" placeholder="Mobile No." />
+                    <input name="phone" onChange={inputHandler} required value={data.phone} type="tel" placeholder="Mobile No." />
                   </div>
                 </div>
                 <div className="row">
                   <div className="col">
-                    <input type="password" placeholder="Password" />
+                    <input name="password" onChange={inputHandler} required value={data.password} type="password" placeholder="Password" />
                   </div>
                   <div className="col">
-                    <input type="text" placeholder="Confirm Password" />
+                    <input name="cpassword" onChange={inputHandler} required value={data.cpassword} type="password" placeholder="Confirm Password" />
                   </div>
                 </div>
-                <button className="opacity mt-3 mb-0">SUBMIT</button>
+                <button className="opacity mt-3 mb-0" type="submit">SUBMIT</button>
               </form>
               <div className="r-forget opacity">
                 <h6>
@@ -47,7 +81,6 @@ export default function Register() {
           </div>
           <div className="theme-btn-container"></div>
         </section>
-      </body>
     </div>
   );
 }
