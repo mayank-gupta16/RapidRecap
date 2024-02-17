91bb631d77f411cbc5d1c06d223ef2ea8cddbe37
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-02-17 14:22:46 +0530

    Added IQ_score field

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -51,6 +51,10 @@ const userSchema = new mongoose.Schema(
         ref: "QUIZ_ATTEMPT",
       },
     ],
+    IQ_score: {
+      type: Number,
+      default: 0,
+    },
   },
   { collection: "Users" }
 );
