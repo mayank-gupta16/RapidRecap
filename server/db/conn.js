2456c6f5c757b892e365b044774ca24ce467d306
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-02-29 10:20:46 +0530

    Added function to add data to database

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const DB = process.env.DATABASE;
+// const Article = require("../model/articleSchema");
+// const articlesData = require("../ArticleHealthData.json");
 
 mongoose
   .connect(DB)
@@ -9,3 +11,19 @@ mongoose
   .catch((err) => {
     console.log(`connection unsuccessful`);
   });
+
+// async function saveArticlesToDB() {
+//   try {
+//     for (const articleData of articlesData) {
+//       const article = new Article(articleData);
+//       await article.save();
+//       console.log(`Article saved: ${article.title}`);
+//     }
+//     console.log("All articles saved successfully!");
+//   } catch (error) {
+//     console.error("Error saving articles:", error);
+//   }
+// }
+
+// // Call the function to save articles to the database
+// saveArticlesToDB();
