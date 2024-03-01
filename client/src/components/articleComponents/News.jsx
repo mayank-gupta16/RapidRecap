8be154a209d3c9799fe8a8ff4d9aace29f3b2b4c
Subrat <subratgupta2704@gmail.com>
2024-03-01 14:53:07 +0530

    News.jsx CSS updated

--- client/src/components/articleComponents/News.jsx
+++ client/src/components/articleComponents/News.jsx
@@ -3,6 +3,7 @@ import { AppContext } from "../../contextAPI/appContext";
 import { Button, Image } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import Alt_img from "../../assets/alt_image.jpg";
+
 const News = () => {
   const { state, dispatch } = useContext(AppContext);
   const data = state.news;
@@ -11,25 +12,37 @@ const News = () => {
   useEffect(() => {
     if (window.innerWidth < 768) navigate(`/article/${data._id}`);
   }, [window.innerWidth]);
+
   return (
-    <div className="d-flex justify-content-center flex-column align-items-center text-white">
+    <div className="d-flex justify-content-center flex-column align-items-center" style={{ backgroundColor: "#1a1a2e", color: "#253547", minHeight: "90vh" }}>
       <Image
         src={data.imgURL[0]}
-        style={{ width: "95%" }}
+        style={{ width: "95%", marginTop: "1rem"}}
         onError={(e) => {
           e.target.onerror = null;
           e.target.src = Alt_img;
         }}
       />
-      <div style={{ width: "80%" }}>
-        <h1 className="mt-2 mb-5">{data.title}</h1>
-        <p className="mb-4">
+      <div style={{ width: "92%"}}>
+        <h1 className="mt-3 mb-3" style={{ color: "#f0f0f0", textAlign: "center" }}>{data.title}</h1>
+        <p className="mb-2" style={{ color: "#f0f0f0", textAlign: "justify" }}>
           {data.mainText.length > 1000
             ? `${data.mainText.substring(0, 1000)}...`
             : data.mainText}
         </p>
         <Link to={`/article/${data._id}`}>
-          <Button colorScheme="teal">Source</Button>
+          <Button  mb={2} 
+            style={{ 
+              maxWidth: "100%", 
+              transition: "background-color 0.3s, color 0.3s", 
+              backgroundColor: "#253547", 
+              borderColor: "#f0f0f0",
+            }}
+            onMouseOver={(e) => { e.currentTarget.style.backgroundColor = "#37474f"; e.currentTarget.style.color = "#f0f0f0"; }}
+            onMouseOut={(e) => { e.currentTarget.style.backgroundColor = "#253547"; e.currentTarget.style.color = "#f0f0f0"; }}
+          >
+            Read More
+          </Button>
         </Link>
       </div>
     </div>
