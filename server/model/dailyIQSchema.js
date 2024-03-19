5f808f689536ef777234b12ef190c06684efb31f
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-03-19 12:52:49 +0530

    dailyIQSchema added to track history

---
+++ server/model/dailyIQSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+// Daily IQ Score Schema
+const dailyIQSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  IQ_score: {
+    type: Number,
+    required: true,
+  },
+  dailyRank: {
+    type: String,
+    required: true,
+  },
+});
+
+const DailyIQ = mongoose.model("DailyIQ", dailyIQSchema); // Daily IQ model
+
+module.exports = { DailyIQ };
