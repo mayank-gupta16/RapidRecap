f268bb4e8e1baf5b1ecc9ba89e88080a5630d46f
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-03-19 21:02:01 +0530

    Added logic to test IQ score history of a user

--- server/test/index.js
+++ server/test/index.js
@@ -1,8 +1,13 @@
 const generateArticlesQuizzesAndQuizAttempts = require("../utils/test.utils/generateArticlesQuizzesAndQuizAttempts.test"); // Import generateArticlesQuizzesAndQuizAttempts utility function
-
+const generateIQScoreHistory = require("../utils/test.utils/generateIQScoreHistory.test"); // Import generateIQScoreHistory utility function
 // Call the main function
-generateArticlesQuizzesAndQuizAttempts()
+generateIQScoreHistory()
   .then(() => {
-    console.log("Articles, quizzes, and quiz attempts generated successfully");
+    console.log("IQ scores history generated successfully");
   })
   .catch((err) => console.error(err));
+// generateArticlesQuizzesAndQuizAttempts()
+//   .then(() => {
+//     console.log("Articles, quizzes, and quiz attempts generated successfully");
+//   })
+//   .catch((err) => console.error(err));
