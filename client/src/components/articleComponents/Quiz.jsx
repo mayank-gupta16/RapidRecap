f16498890849e94712b7b5fe4db29907bc5bdd43
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-03-20 11:16:21 +0530

    Refactored quiz component into smaller components

--- client/src/components/articleComponents/Quiz.jsx
+++ client/src/components/articleComponents/Quiz.jsx
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AppContext } from "../../contextAPI/appContext";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Modal,
@@ -8,16 +7,9 @@ import {
   ModalFooter,
   ModalOverlay,
   ModalCloseButton,
-  Grid,
-  GridItem,
   Text,
-  Box,
-  Image,
-  Progress,
   Flex,
   ModalHeader,
-  SlideFade,
-  Heading,
   Skeleton,
   SkeletonCircle,
   useToast,
@@ -27,9 +19,10 @@ import Countdown from "./Countdown";
 import axios from "axios";
 import ConfirmationModal from "./customQuizModal/ConfirmationModal";
 import InstructionModal from "./customQuizModal/InstructionModal";
+import QuizInterface from "./quizComponents/quizInterface";
+import SubmittedQuizInterface from "./quizComponents/SubmittedQuizInterface";
 
 const Quiz = ({ article, isOpen, onClose, ofShowQuiz }) => {
-  const { state, dispatch } = useContext(AppContext);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [submitted, setSubmitted] = useState(false);
   const [userAnswers, setUserAnswers] = useState([]);
@@ -155,7 +148,7 @@ const Quiz = ({ article, isOpen, onClose, ofShowQuiz }) => {
       toast({
         title: "Quiz Generation Failed!",
         description:
-          error.response.data.error ||
+          error.response?.data?.error ||
           error ||
           "Please try again (Close this window and Try refreshing the page)",
         status: "error",
@@ -295,148 +288,17 @@ const Quiz = ({ article, isOpen, onClose, ofShowQuiz }) => {
             >
               {!submitted ? (
                 <>
-                  <Skeleton w={"100%"} borderRadius={"10px"} isLoaded={!load}>
-                    <Flex
-                      width={"100%"}
-                      justifyContent={"center"}
-                      gap={"10px"}
-                      alignItems={"center"}
-                      marginBottom={"20px"}
-                    >
-                      <Progress
-                        hasStripe
-                        value={(currentQuestionIndex / totalQuestions) * 100}
-                        width={{ base: "100%", md: "80%" }}
-                        height={"10px"}
-                        marginBottom={"20px"}
-                        borderRadius={"50px"}
-                        colorScheme="blue"
-                        marginY={"auto"}
-                      />
-
-                      <Text
-                        marginY={"auto"}
-                        color={"white"}
-                        textAlign={"center"}
-                      >
-                        {`${currentQuestionIndex} / ${totalQuestions}`}
-                      </Text>
-                    </Flex>
-                  </Skeleton>
-                  <Skeleton
-                    isLoaded={!load}
-                    display={!load ? "none" : "flex"}
-                    marginBottom={load ? "10px" : "0"} // remove this when skeleton isLoaded
-                    marginTop={load ? "10px" : "0"} // remove this when skeleton isLoaded
-                    borderRadius={"10px"}
-                    flexDirection={"column"}
-                    alignItems={"center"}
-                    minHeight={"300px"}
-                    width={"100%"}
+                  <QuizInterface
+                    load={load}
+                    currentQuestionIndex={currentQuestionIndex}
+                    totalQuestions={totalQuestions}
+                    quizData={quizData}
+                    handleAnswer={handleAnswer}
+                    userAnswers={userAnswers}
                   />
-                  {quizData !== null && (
-                    <>
-                      {/* <Image
-                        src={article.imgURL}
-                        alt="Article Image"
-                        borderRadius="md"
-                        marginBottom="5"
-                        width={{ base: "100%", md: "50%" }}
-                        height="auto"
-                      /> */}
-                      <Text
-                        p={2}
-                        letterSpacing={0.5}
-                        marginBottom={"50px"}
-                        overflowWrap="break-word"
-                        color={"black"}
-                        fontSize={"20px"}
-                        userSelect={"none"}
-                      >
-                        {quizData.questions.length > 0
-                          ? quizData.questions[currentQuestionIndex].question
-                          : ""}
-                      </Text>
-                      <Grid templateColumns={{ md: "1fr 1fr" }} gap="25px">
-                        {Object.entries(
-                          quizData.questions.length > 0
-                            ? quizData.questions[currentQuestionIndex].options
-                            : {}
-                        ).map(([optionKey, optionText]) => (
-                          <GridItem key={optionKey} display={"flex"}>
-                            <Box
-                              display={"flex"}
-                              flexDirection={"row"}
-                              alignItems={"center"}
-                              marginRight={2}
-                              color={"black"}
-                              fontWeight={"bold"}
-                              minW={"25px"}
-                              userSelect={"none"}
-                            >{`${optionKey.toLocaleUpperCase()} :`}</Box>
-                            <Button
-                              border={"1px solid lightgray"}
-                              overflowWrap="break-word"
-                              display={"flex"}
-                              whiteSpace="normal"
-                              justifyContent={"flex-start"}
-                              onClick={() => handleAnswer(optionKey)}
-                              bg={
-                                userAnswers[currentQuestionIndex] === optionKey
-                                  ? "#DDE6ED" // Background color when selected
-                                  : "#183D3D" // Default background color
-                              }
-                              variant={"outline"}
-                              width={"100%"}
-                              maxWidth={"400px"}
-                              textAlign={"left"}
-                              height={"auto"}
-                              px={2}
-                              py={2}
-                              color={
-                                userAnswers[currentQuestionIndex] === optionKey
-                                  ? "#27374D" // Text color when selected
-                                  : "#FAF0E6" // Default text color
-                              }
-                              _hover={{ color: "#163020", bg: "#ADC4CE" }} // Hover effect
-                            >
-                              {`${optionText}`}
-                            </Button>
-                          </GridItem>
-                        ))}
-                      </Grid>
-                    </>
-                  )}
                 </>
               ) : (
-                <SlideFade
-                  direction="bottom"
-                  in={isOpen}
-                  offsetY="20px"
-                  style={{ zIndex: 10 }}
-                >
-                  <Heading
-                    as="h3"
-                    size="lg"
-                    color="#3E3232"
-                    position="absolute"
-                    top="40%"
-                    width="100%"
-                    left="50%"
-                    transform="translateX(-50%)"
-                    textAlign="center"
-                  >
-                    Quiz completed. Thank you for participating!
-                  </Heading>
-                  <Text
-                    color="#503C3C"
-                    fontSize="30px"
-                    textAlign="center"
-                    marginTop="10"
-                  >
-                    RQM_score: {`${score}`}
-                  </Text>
-                </SlideFade>
+                <SubmittedQuizInterface isOpen={isOpen} score={score} />
               )}
             </ModalBody>
           )}
