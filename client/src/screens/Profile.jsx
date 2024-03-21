ffbfd815e992d9418ce2c31bb57d1a07a665c9ed
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-03-21 20:55:26 +0530

    Made responsive

--- client/src/screens/Profile.jsx
+++ client/src/screens/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { AppContext } from "../contextAPI/appContext";
 import IQLineGraph from "../components/profileComponents/IQLineGraph";
 import IQBarGraph from "../components/profileComponents/IQBarGraph";
@@ -17,13 +17,20 @@ export default function Profile() {
 
   return (
     <>
-      <Box display="flex" marginTop="20px" marginInline={"6.5%"}>
+      <Flex
+        flexDirection={{ base: "column", md: "row" }}
+        marginTop="20px"
+        marginInline={{ base: "0", xl: "6.5%" }}
+        alignItems={{ base: "center", md: "normal" }}
+        justifyContent={{ base: "center", md: "center", lg: "normal" }}
+      >
         <Flex
           margin="20px"
           padding="15px"
           borderRadius="10px"
           flexDirection="column"
-          w={"25%"}
+          w={{ md: "300px", lg: "350px", base: "95%" }}
+          height="fit-content"
           style={{
             backgroundColor: "#0f0d15",
             backgroundImage:
@@ -35,34 +42,45 @@ export default function Profile() {
           <LeftProfileBox />
         </Flex>
         <Flex
-          w={"75%"}
+          w={{
+            xl: "calc(100% - 400px)",
+            md: "calc(100% - 460px)",
+            sm: "100%",
+            base: "100%",
+          }}
           flexDirection="column"
           margin="12px"
+          justifyContent={"center"}
+          alignItems={"center"}
           borderRadius="10px"
         >
-          <Box
-            margin="10px"
-            padding="20px"
+          <Flex
+            w={"100%"}
+            marginTop={"10px"}
+            marginInline={"1%"}
+            padding={{ xl: "20px", base: "0" }}
             borderRadius="10px"
-            display={"flex"}
-            flexDirection={"row"}
-            style={{
-              backgroundColor: "#0f0d15",
-              backgroundImage:
-                "linear-gradient(-180deg, #1a1527, #0e0c16 88%, #0e0c16 99%)",
-              boxShadow:
-                "0px 4px 8px rgba(0, 0, 0, 0.3), 0px 8px 16px rgba(0, 0, 0, 0.3), 0px 12px 24px rgba(0, 0, 0, 0.3)", // Increased intensity of the shadow
+            flexDirection={{ base: "column", xl: "row" }}
+            backgroundColor={{ base: "transparent", xl: "#0f0d15" }}
+            backgroundImage={{
+              base: "none",
+              xl: "linear-gradient(-180deg, #1a1527, #0e0c16 88%, #0e0c16 99%)",
             }}
+            boxShadow={{
+              base: "none",
+              xl: "0px 4px 8px rgba(0, 0, 0, 0.3), 0px 8px 16px rgba(0, 0, 0, 0.3), 0px 12px 24px rgba(0, 0, 0, 0.3)",
+            }}
+            gap={{ base: "20px", xl: "0" }}
           >
             <IQLineGraph />
             <IQBarGraph />
-          </Box>
+          </Flex>
 
           <Flex
+            w={"100%"}
             margin="10px"
-            marginTop="5px"
             marginBottom="5px"
-            flexDirection="row"
+            flexDirection={{ xl: "row", base: "column" }}
             justifyContent="space-between"
             gap={5}
           >
@@ -93,22 +111,23 @@ export default function Profile() {
               <RankAndSociety />
             </Flex>
           </Flex>
+          <Box
+            w={"100%"}
+            margin="10px"
+            p={"10px"}
+            borderRadius="10px"
+            style={{
+              backgroundColor: "#0f0d15",
+              backgroundImage:
+                "linear-gradient(-180deg, #1a1527, #0e0c16 88%, #0e0c16 99%)",
+              boxShadow:
+                "0px 4px 8px rgba(0, 0, 0, 0.3), 0px 8px 16px rgba(0, 0, 0, 0.3), 0px 12px 24px rgba(0, 0, 0, 0.3)", // Increased intensity of the shadow
+            }}
+          >
+            <DailyActivity />
+          </Box>
         </Flex>
-      </Box>
-      <Box
-        p={"20px"}
-        marginInline={"7.5%"}
-        borderRadius="10px"
-        style={{
-          backgroundColor: "#0f0d15",
-          backgroundImage:
-            "linear-gradient(-180deg, #1a1527, #0e0c16 88%, #0e0c16 99%)",
-          boxShadow:
-            "0px 4px 8px rgba(0, 0, 0, 0.3), 0px 8px 16px rgba(0, 0, 0, 0.3), 0px 12px 24px rgba(0, 0, 0, 0.3)", // Increased intensity of the shadow
-        }}
-      >
-        <DailyActivity />
-      </Box>
+      </Flex>
     </>
   );
 }
