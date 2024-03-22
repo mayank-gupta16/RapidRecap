560d160d61867bcc8b328a4c868a8bc18f5fcde9
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-03-22 10:52:37 +0530

    Added data to show the society and circle

---
+++ client/src/assets/CircleAndSocietyData.js
@@ -0,0 +1,72 @@
+import Mavericks_Brain from "/images/Mavericks_Brain.png";
+import Explorers_Brain from "/images/Explorers_Brain.png";
+import Strivers_Brain from "/images/Strivers_Brain.png";
+import Elites_Brain from "/images/Elites_Brain.png";
+import Titans_Brain from "/images/Titans_Brain.png";
+const CircleAndSocietyData = [
+  {
+    society: "Titans Society",
+    circle: null,
+    image: Titans_Brain,
+    IQ_Lower: 150,
+    IQ_Upper: null,
+  },
+  {
+    society: "Mavericks Society",
+    circle: "Visionaries Circle",
+    image: Mavericks_Brain,
+    IQ_Lower: 140,
+    IQ_Upper: 150,
+  },
+  {
+    society: "Mavericks Society",
+    circle: "Pioneers Circle",
+    image: Mavericks_Brain,
+    IQ_Lower: 130,
+    IQ_Upper: 140,
+  },
+  {
+    society: "Elites Society",
+    circle: "Scholars Circle",
+    image: Elites_Brain,
+    IQ_Lower: 120,
+    IQ_Upper: 130,
+  },
+  {
+    society: "Elites Society",
+    circle: "Masters Circle",
+    image: Elites_Brain,
+    IQ_Lower: 110,
+    IQ_Upper: 120,
+  },
+  {
+    society: "Strivers Society",
+    circle: "Enthusiasts Circle",
+    image: Strivers_Brain,
+    IQ_Lower: 104,
+    IQ_Upper: 110,
+  },
+  {
+    society: "Strivers Society",
+    circle: "Achievers Circle",
+    image: Strivers_Brain,
+    IQ_Lower: 97,
+    IQ_Upper: 104,
+  },
+  {
+    society: "Strivers Society",
+    circle: "Progressors Circle",
+    image: Strivers_Brain,
+    IQ_Lower: 90,
+    IQ_Upper: 97,
+  },
+  {
+    society: "Explorers Society",
+    circle: null,
+    image: Explorers_Brain,
+    IQ_Lower: 0,
+    IQ_Upper: 90,
+  },
+];
+
+export default CircleAndSocietyData;
